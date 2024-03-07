35a034fd94ec2f8687255af68604d99223446bf2
irungundigirigi <irungundigirigi@gmail.com>
2024-03-08 01:37:04 +0300

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    console.log(this.#evacuationWarningMessage())
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(sqft) {
-    this._sqft = sqft;
+  set sqft(value) {
+    this._sqft = value;
   }
-
- #evacuationWarningMessage(){
-  return 'Hey There'
- }
 }
