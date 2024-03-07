4c6678d839045d49ce6030223fe97c0609bd417a
irungundigirigi <irungundigirigi@gmail.com>
2024-03-08 02:45:13 +0300

    update

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,30 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(code) {
-    this._code = code;
+  set code(value) {
+    this._code = value;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(name) {
-    this._code = name;
+  set name(value) {
+    this._name = value;
   }
 
+  /**
+   * Creates the full string representation of this Currency.
+   * @returns {String}
+   */
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
