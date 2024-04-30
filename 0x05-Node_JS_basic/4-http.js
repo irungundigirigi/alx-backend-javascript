91c1e47a22257bbd86f6d8deb4eb020d3257cfd1
edwin <irungundigirigi@gmai.com>
2024-04-30 18:01:59 +0300

    a small server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,7 +7,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = '1245';
 
-const app = http.createserver((request, response) => {
+const app = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
   response.end('Hello Holberton School!');
