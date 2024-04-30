3b27a2ec36beeb85c81b840bd882c4d6b0f23f16
edwin <irungundigirigi@gmai.com>
2024-04-30 13:41:19 +0300

    fix linting issues

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,8 @@
+/**
+ * This script prompts the user to enter their name and displays it back.
+ * If executed in a TTY environment, it reads from stdout. Otherwise, it reads from stdin.
+ */
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
@@ -9,8 +14,9 @@ if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-  })
+  });
+  
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
-  })
+  });
 }
